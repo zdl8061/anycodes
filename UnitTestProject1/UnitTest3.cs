6697ad501d38c231a6fefd1f1787b4077e0b9c75
zhangdeliang <zdl8061@163.com>
2016-11-14 09:03:47 +0800

    1

--- UnitTestProject1/UnitTest3.cs
+++ UnitTestProject1/UnitTest3.cs
@@ -2,6 +2,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Web;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using ZDL.AnyCodes;
+using System.Dynamic;
+using System.Collections;
 
 namespace UnitTestProject1
 {
@@ -11,31 +17,39 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            //using (TxDataHelper helper = TxDataHelper.GetDataHelper("TxoooAgent"))
-            //{
-            //    string _sql = "select * from sales_product_index";
+            var _root = new JObject();
+            var _get = new JObject{
+                  { "@action", "handle-user-input.jsp" },
+                  { "@numdigits", "1"},
+                  { "Play", "menu.wav" }
+            };
+            _root.Add("Get", _get);
+            _root.Add("Play", "sorrybye.wav");
+            _root.Add("Redirect", "/welcome/voice");
+            var _res = new JObject();
+            _res.Add("Response", _root);
 
-            //    DataTable _dt = helper.SqlGetDataTable(_sql);
+            var c = JsonConvert.DeserializeXNode(_res.ToString());
+            var s = JsonConvert.SerializeXNode(c);
 
-            //    string json = JsonConvert.SerializeObject(_dt);
+            //{ "Response":{ "Get":{ "@action":"handle-user-input.jsp","@numdigits":"1","Play":"menu.wav"},"Play":"sorrybye.wav","Redirect":"/welcome/voice"} }
 
-            //    string cjson = ZipHelper.Compress(json);
-            //}
+            var _n = new JObject
+            {
+                {"Response",new JObject
+                {
+                    { "Get",new JObject
+                    {
+                        { "@action","handle-user-input.jsp"},
+                        { "@numdigits",1},
+                        { "Play","menu.wav"}
+                    } },
+                    { "Play","sorrybye.wav"},
+                    { "Redirect","/welcome/voice"}
+                } }
+            };
 
-            //var _root = new JObject();
-            //var _get = new JObject{
-            //      { "@action", "handle-user-input.jsp" },
-            //      { "@numdigits", "1"},
-            //      { "Play", "menu.wav" }
-            //};
-            //_root.Add("Get", _get);
-            //_root.Add("Play", "sorrybye.wav");
-            //_root.Add("Redirect", "/welcome/voice");
-            //var _res = new JObject();
-            //_res.Add("Response", _root);
-
-            //var c = JsonConvert.DeserializeXNode(_res.ToString());
-            //var s = JsonConvert.SerializeXNode(c);
+            var t = _n.ToString();
 
 
 
@@ -51,7 +65,21 @@ namespace UnitTestProject1
 
             var d = s.AllKeys.ToDictionary(k => k, k => s[k]);
 
-            //var _r = JsonConvert.SerializeObject(d);
+            var _r = JsonConvert.SerializeObject(d);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+
+            string _sql = "select * from test where name=@p0 and age=@p1 and tt=@p2";
+
+
+            //TxDataV2.SpInsert("TxoooCMS", "SP_ZDL_InsertNews", new Hashtable
+            //{
+            //    { "@add","111"}
+            //});
+
         }
     }
 }
---
+++ ZDL.AnyCodes/TxDataV2.cs
@@ -0,0 +1,88 @@
+﻿/*----------------------------------------------------------------
+ *  Copyright (C) 2016 天下商机（txooo.com）版权所有
+ * 
+ *  文 件 名：TxDataV2
+ *  所属项目：
+ *  创建用户：张德良
+ *  创建时间：2016/11/11 星期五 下午 15:27:26
+ *  
+ *  功能描述：
+ *          1、
+ *          2、
+ * 
+ *  修改标识：
+ *  修改描述：
+ *  待 完 善：
+ *          1、 
+----------------------------------------------------------------*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Linq.Mapping;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Txooo;
+
+namespace ZDL.AnyCodes
+{
+    public class TxDataV2
+    {
+        public static int SqlInsert(string dataBase, string sql, params object[] sqlParams)
+        {
+            try
+            {
+                var _hashTable = new Hashtable();
+
+                var _matches = Regex.Matches(sql, "@p\\d", RegexOptions.IgnoreCase);
+                for (int i = 0; i < _matches.Count; i++)
+                {
+                    _hashTable[_matches[i].Value] = sqlParams[i];
+                }
+
+                using (Txooo.TxDataHelper helper = Txooo.TxDataHelper.GetDataHelper(dataBase))
+                {
+                    helper.SpFileValue = _hashTable;
+
+                    return helper.SqlExecute(sql, helper.SpFileValue);
+                }
+            }
+            catch (Exception ex)
+            {
+                TxLogHelper.GetLogger("TxDataV2").Error(ex.Source + "|" + ex.TargetSite + "|" + ex.Message);
+
+                return 0;
+            }
+        }
+
+        public static DataTable SqlSelect(string dataBase, string sql, params object[] sqlParams)
+        {
+            try
+            {
+                var _hashTable = new Hashtable();
+
+                var _matches = Regex.Matches(sql, "@p\\d", RegexOptions.IgnoreCase);
+                for (int i = 0; i < _matches.Count; i++)
+                {
+                    _hashTable[_matches[i].Value] = sqlParams[i];
+                }
+
+                using (Txooo.TxDataHelper helper = Txooo.TxDataHelper.GetDataHelper(dataBase))
+                {
+                    helper.SpFileValue = _hashTable;
+
+                    return helper.SqlGetDataTable(sql, helper.SpFileValue);
+                }
+            }
+            catch (Exception ex)
+            {
+                TxLogHelper.GetLogger("TxDataV2").Error(ex.Source + "|" + ex.TargetSite + "|" + ex.Message);
+
+
+            }
+            return null;
+        }
+    }
+}
