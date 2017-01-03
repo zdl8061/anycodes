bca1e43a8b12e0eaed148040dcd05038af5a1533
zhangdeliang <zdl8061@163.com>
2017-01-03 15:51:17 +0800

    1

---
+++ WindowsService1/bat/install.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -i "WindowsService1.exe"
+@pause
---
+++ WindowsService1/bat/uninstall.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe -u "WindowsService1.exe"
+@pause
