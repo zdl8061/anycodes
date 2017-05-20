ea13464b78d0c2e61ee82ad9ffd4aa16fc4a9684
zdl8061 <zdl8061@163.com>
2017-05-20 21:57:00 +0800

    1

---
+++ UnitTestProject1/AppBmPush/Domain/Entities/PushConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.AppBmPush
+{
+    public class PushConfig
+    {
+        public int BrandId { get; set; }
+
+        public bool IsEnable { get; set; }
+
+        public int NexId { get; set; }
+
+        public string OutTypes { get; set; }
+
+        public string ReceiveUsers { get; set; }
+
+        public int StartHour { get; set; }
+
+        public int EndHour { get; set; }
+    }
+}
---
+++ UnitTestProject1/AppBmPush/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestProject1.AppBmPush
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IList<ClueInfo> _clueList =
+        }
+    }
+}
