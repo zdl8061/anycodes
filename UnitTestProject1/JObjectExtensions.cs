5d42dd348c3647711439e77c2aa295383ceec6e9
zhangdeliang <zhangdeliang@163.com>
2017-05-26 16:56:16 +0800

    1

--- UnitTestProject1/JObjectExtensions.cs
+++ UnitTestProject1/JObjectExtensions.cs
@@ -88,4 +88,5 @@ namespace UnitTestProject1
         }
 
     }
+    
 }
--- UnitTestProject1/MongoTestSamus.cs
+++ UnitTestProject1/MongoTestSamus.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using MongoDB;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace UnitTestProject1
 {
@@ -12,13 +14,31 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestMethod1()
         {
-            //MongoHelper.InsertOne<ChatInfo>("chatlog", new ChatInfo { Name = "open", Email = "open@txooo.com", Level = 1 });
+            var _chat = new ChatInfo()
+            {
+                Name = "asdf",
+                Email = "hu2@dsf.com",
+                Level = 2,
+                Link = new LinkInfo()
+                {
+                    Family = "fengtaiqu ",
+                    Mobile = "13563212254"
+                },
+                LinkCols = new List<LinkInfo>()
+                {
+                    new LinkInfo{  Family="lskdjfie", Mobile="15325632125"},
+                    new LinkInfo{ Family="erjoeijrgeerog", Mobile="18654512215"}
+                }
+            };
 
-            var _json = JObject.Parse(JsonConvert.SerializeObject(new ChatInfo { Name = "open", Email = "open@txooo.com", Level = 1 }));
+           
+            MongoHelper.InsertOne<ChatInfo>("chatlog", _chat);
 
-            var _doc = new Document(_json.ToDictionary());
+            //var _json = JObject.Parse(JsonConvert.SerializeObject(_chat));
 
-            MongoHelper.InsertOne<Document>("chatlog", _doc);
+            //var _doc = new Document();//_json.ToDictionary()           
+
+            //MongoHelper.InsertOne<Document>("chatlog", _doc);
         }
     }
 
@@ -30,6 +50,15 @@ namespace UnitTestProject1
 
         public int Level { get; set; }
 
+        public LinkInfo Link { get; set; }
 
+        public IList<LinkInfo> LinkCols { get; set; }
+    }
+
+    public class LinkInfo
+    {
+        public string Mobile { get; set; }
+
+        public string Family { get; set; }
     }
 }
