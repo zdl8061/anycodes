69695aa8173eff2529d26cd5cc34d560f181abad
zhangdeliang <zhangdeliang@163.com>
2017-08-15 17:25:20 +0800

    1

---
+++ DotNet.Utilities.Test/LimitPropsContractResolver.cs
@@ -0,0 +1,65 @@
+﻿/*----------------------------------------------------------------
+ *  Copyright (C) 2015 天下商机（txooo.com）版权所有
+ * 
+ *  文 件 名：Class1
+ *  所属项目：
+ *  创建用户：张德良
+ *  创建时间：2017/8/15 星期二 下午 16:53:57
+ *  
+ *  功能描述：
+ *          1、
+ *          2、 
+ * 
+ *  修改标识：  
+ *  修改描述：
+ *  待 完 善：
+ *          1、 
+----------------------------------------------------------------*/
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet.Utilities.Test
+{
+    public class LimitPropsContractResolver : DefaultContractResolver
+    {
+        string[] props = null;
+
+        bool retain;
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="props">传入的属性数组</param>
+        /// <param name="retain">true:表示props是需要保留的字段  false：表示props是要排除的字段</param>
+        public LimitPropsContractResolver(string[] props, bool retain = true)
+        {
+            //指定要序列化属性的清单
+            this.props = props;
+
+            this.retain = retain;
+        }
+
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            IList<JsonProperty> list =
+            base.CreateProperties(type, memberSerialization);
+            //只保留清单有列出的属性
+            return list.Where(p =>
+            {
+                if (retain)
+                {
+                    return props.Contains(p.PropertyName);
+                }
+                else
+                {
+                    return !props.Contains(p.PropertyName);
+                }
+            }).ToList();
+        }
+    }
+}
--- DotNet.Utilities.Test/SqlSugarTest1.cs
+++ DotNet.Utilities.Test/SqlSugarTest1.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SqlSugar;
 using Txooo;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace DotNet.Utilities.Test
 {
@@ -12,9 +14,13 @@ namespace DotNet.Utilities.Test
         public void TestMethod1()
         {
             using (var db = GetInstance("DapperDemo"))
-            {
-                var _userList = db.Ado.SqlQuery<UserInfo>("SELECT * FROM Users");
-                //var _userList = db.Queryable<UserInfo>().ToList();
+            {                
+                //var _userList = db.Ado.SqlQuery<UserInfo>("SELECT * FROM Users");//字段名和表明必须和库一样，映射没用
+                var _userList = db.Queryable<UserInfo>().ToList();
+
+                JsonSerializerSettings jsetting = new JsonSerializerSettings();
+                jsetting.ContractResolver = new LimitPropsContractResolver(new string[] { "UserId", "Username" , "Regtime", "MyPermission" , "PermissionId", "PermissionName" });
+                var _json = JsonConvert.SerializeObject(_userList, jsetting);
             }
         }
 
@@ -27,9 +33,11 @@ namespace DotNet.Utilities.Test
         }
     }
 
-    public class UserInfo
+    [SugarTable("Users")]
+    public class UserInfo : ModelContext
     {
-        [SugarColumn(ColumnName ="user_id")]
+
+        [SugarColumn(ColumnName = "user_id")]
         public int UserId { get; set; }
 
         [SugarColumn(ColumnName = "user_name")]
@@ -40,5 +48,49 @@ namespace DotNet.Utilities.Test
 
         [SugarColumn(ColumnName = "reg_time")]
         public DateTime Regtime { get; set; }
+
+
+        [SugarColumn(IsIgnore = true)]
+        public List<UserPermission> MyPermission
+        {
+            get
+            {
+                return  base.CreateMapping<UserPermission>().Where(p => p.UserId == this.UserId).ToList();
+            }
+        }
+    }
+
+    [SugarTable("UserPermissions")]
+    public class UserPermission: ModelContext
+    {
+        [SugarColumn(ColumnName = "userid")]
+        public int UserId { get; set; }
+
+        [SugarColumn(ColumnName = "perm_id")]
+        public int PermissionId { get; set; }
+
+
+        [SugarColumn(IsIgnore = true)]
+        public string PermissionName
+        {
+            get {
+                var perm = base.CreateMapping<Permission>().First(p => p.PermissionId == this.PermissionId);
+
+                return perm.PermissionName;
+            }
+        }
+    }
+
+    [SugarTable("Permissions")]
+    public class Permission : ModelContext
+    {
+        [SugarColumn(ColumnName = "perm_id")]
+        public int PermissionId { get; set; }
+
+        [SugarColumn(ColumnName = "perm_name")]
+        public string PermissionName { get; set; }
+
+        [SugarColumn(ColumnName = "perm_url")]
+        public string PermissionUrl { get; set; }
     }
 }
