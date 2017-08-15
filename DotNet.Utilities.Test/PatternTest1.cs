543e1b8fea661a2ea1810eaebfe01b4e32e61fec
zhangdeliang <zhangdeliang@163.com>
2017-08-15 15:39:31 +0800

    1

---
+++ DotNet.Utilities.Test/PatternTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNet.Utilities.Test
+{
+    [TestClass]
+    public class PatternTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+
+    public class EmployeeInfo
+    {
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public void Befire(Visitor visitor)
+        {
+            //visitor.Visit(this);
+        }
+    }
+
+    public class Manager : Visitor
+    {
+        public void Visit(Manager manage)
+        {
+
+        }
+    }
+
+    public class Visitor
+    {
+       //abstract void Visit()
+    }
+}
